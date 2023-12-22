2bed35697dc37a8ac1804ab626dc5ad685f2647a
matu184 <matu184@live.de>
2023-12-22 14:30:19 +0100

    M1

---
+++ FractionsProject/Fraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionsProject
+{
+    internal class Fraction
+    {
+        int numerator;
+        int denominator;
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
---
+++ FractionsProject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FractionsProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fraction f = new Fraction(1, 2);
+        Console.WriteLine(f);
+        Console.ReadLine();
+
+    }
+}
